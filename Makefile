030fe14cb234087477c0ee0ad48ad641d6415fcd
Tilo Geissler <tilo.geissler@sap.com>
2019-07-24 16:35:31 +0200

    fixes output sub paths

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 
 IMAGE:=hub.global.cloud.sap/monsoon/grafana-minitrue
-VERSION:=v0.0.1
+VERSION:=v0.0.4
 #VERSION:=latest
-#VERSION:=v0.0.3
+
 
 docker-build:
 	docker build -t $(IMAGE):$(VERSION) .
 
-docker-push:
+docker-push: docker-build
 	docker push $(IMAGE):$(VERSION)
--- minitrue.sh
+++ minitrue.sh
@@ -1,16 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-PATH_IN=${PATH_IN:-/gitsync_in}
-PATH_OUT=${PATH_OUT:-/minitrue_out}
+PATH_IN=${PATH_IN:-/gitsync_in/grafana-content}
+PATH_OUT=${PATH_OUT:-/minitrue_out/grafana-content}
 SLEEP_TIME_IN_SEC=${SLEEP_TIME_IN_SEC:-60}
 
+# ensure existance of out dirs
+cd ${PATH_IN}
+mkdir -p ${PATH_OUT}
+for i in $(find dashboards-* -type d); do
+  mkdir -p ${PATH_OUT}/$i
+done
+
+# loop and convert
 while true
 do
-  cd ${PATH_IN}
   ( set +eo pipefail ;  find . -name \*.json -print0 ; set -eo pipefail ) | while read -d $'\0' i; do
     echo ${i}
     NEW_UID=$(cat "${i}" | jq --raw-output .title | sed 's, -,-,g ; s,- ,-,g ; s, ,-,g' | tr '[:upper:]' '[:lower:]' | head -c 39)
-    sed 's,"uid": .*,"uid": "'${NEW_UID}'",' ${i} > ../${PATH_OUT}/${i}
+    sed 's,"uid": .*,"uid": "'${NEW_UID}'",' ${i} > ${PATH_OUT}/${i}
   done
   sleep ${SLEEP_TIME_IN_SEC}
 done
